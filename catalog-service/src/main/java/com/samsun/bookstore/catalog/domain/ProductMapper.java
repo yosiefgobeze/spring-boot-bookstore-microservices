5451225128d2152d9237b498bf1ef8cadddde211
yosiefgobeze <yosiefgobeze@gmail.com>
2026-04-19 16:53:33 -0700

    Verifying overall progress

--- catalog-service/src/main/java/com/samsun/bookstore/catalog/domain/ProductMapper.java
+++ catalog-service/src/main/java/com/samsun/bookstore/catalog/domain/ProductMapper.java
@@ -1,7 +1,7 @@
 package com.samsun.bookstore.catalog.domain;
 
 class ProductMapper {
-
+    // Later we will use Method Reference to do the mapping
     static Product toProduct(ProductEntity productEntity) {
         return new Product(
                 productEntity.getCode(),
