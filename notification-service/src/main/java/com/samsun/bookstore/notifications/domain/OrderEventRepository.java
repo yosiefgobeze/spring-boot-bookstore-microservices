70a423ec8079f5fa6a35c541175c18eb830d192a
yosiefgobeze <yosiefgobeze@gmail.com>
2026-04-25 23:44:57 -0700

    Fixes on implementation of notification-service

--- notification-service/src/main/java/com/samsun/bookstore/notifications/domain/OrderEventRepository.java
+++ notification-service/src/main/java/com/samsun/bookstore/notifications/domain/OrderEventRepository.java
@@ -3,5 +3,6 @@ package com.samsun.bookstore.notifications.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderEventRepository extends JpaRepository<OrderEventEntity, Long> {
+    // check if eventId exists or not, prevents duplicate entries
     boolean existsByEventId(String eventId);
 }
