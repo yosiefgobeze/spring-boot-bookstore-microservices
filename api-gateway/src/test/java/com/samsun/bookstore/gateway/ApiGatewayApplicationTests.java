e3415034d4bf8e08a3bb3bf69f2a62ba03491300
yosiefgobeze <yosiefgobeze@gmail.com>
2026-04-26 23:22:17 -0700

    Implemented API-gateway

---
+++ api-gateway/src/test/java/com/samsun/bookstore/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.samsun.bookstore.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
